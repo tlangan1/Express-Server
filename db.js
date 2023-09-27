1e43e5fa95f06fd609ebfc783c4bdc436760aaaa
Thomas Langan <tom.langan@comcast.net>
2023-09-26 21:34:27 -0400

    Add delete objective Add more robust client-side error display for debugging Add getGoals

--- db.js
+++ db.js
@@ -6,45 +6,10 @@ var pool = createPool({
   host: "localhost",
   user: "tlangan",
   password: "-UnderAWhiteSky1",
-  database: "todos", //schema
+  database: "life_helper", //schema
   // Remember, connections are lazily created
   connectionLimit: 10,
 });
-
-export function getTodos() {
-  try {
-    return new Promise(func);
-  } catch (err) {
-    console.log("Database error", err);
-  }
-
-  function func(resolve, reject) {
-    pool.query("select * from todos", function (error, rows) {
-      if (error) reject(error);
-      var x = rows.map((item) => item.todo_name);
-      resolve(x);
-    });
-  }
-}
-
-export function getTasks() {
-  try {
-    return new Promise(func);
-  } catch (err) {
-    console.log("Database error", err);
-  }
-
-  function func(resolve, reject) {
-    pool.query("select * from tasks", function (error, rows) {
-      if (error) {
-        reject(new Error(error));
-        return;
-      }
-      var x = rows.map((item) => item.todo_name);
-      resolve(x);
-    });
-  }
-}
 
 export function getObjectives() {
   try {
@@ -54,13 +19,30 @@ export function getObjectives() {
   }
 
   function func(resolve, reject) {
-    pool.query("select * from objective", function (error, rows) {
+    pool.query("Call p_get_objectives", function (error, rows) {
       if (error) {
         reject(new Error(error));
         return;
       }
-      var x = rows.map((item) => item.objective_name);
-      resolve(x);
+      resolve(rows[0]);
+    });
+  }
+}
+
+export function getGoals() {
+  try {
+    return new Promise(func);
+  } catch (err) {
+    console.log("Database error", err);
+  }
+
+  function func(resolve, reject) {
+    pool.query("Call p_get_goals", function (error, rows) {
+      if (error) {
+        reject(new Error(error));
+        return;
+      }
+      resolve(rows[0]);
     });
   }
 }
@@ -75,6 +57,30 @@ export async function addObjective(data) {
       } else {
         con.query(
           `Call p_add_objective('${data.name}', '${data.description}')`,
+          function (err, rows) {
+            if (err) {
+              reject(new Error(err));
+            } else {
+              con.release(); // releasing connection to pool
+              return resolve(rows);
+            }
+          }
+        );
+      }
+    }); // getConnection
+  }
+}
+
+export async function deleteObjective(data) {
+  return new Promise(fn);
+
+  function fn(resolve, reject) {
+    pool.getConnection(function (err, con) {
+      if (err) {
+        return reject(err);
+      } else {
+        con.query(
+          `Call p_delete_objective('${data.item_id}')`,
           function (err, rows) {
             if (err) {
               reject(new Error(err));
--- server.js
+++ server.js
@@ -2,7 +2,12 @@
 // listening for http requests on port 3001
 
 import express from "express";
-import { addObjective, getObjectives } from "./db.js";
+import {
+  addObjective,
+  deleteObjective,
+  getObjectives,
+  getGoals,
+} from "./db.js";
 import cors from "cors";
 import path from "path";
 import { fileURLToPath } from "url";
@@ -17,36 +22,44 @@ app.use(express.json());
 app.use(cors({ origin: "http://127.0.0.1:3000" }));
 
 app.get("*", (req, res) => {
-  console.log("Server Get Request:", req.url);
+  console.log("Server Get Request:", "url is ", req.url, "body is", req.body);
 
   switch (req.url) {
-    case "/objectives":
-      try {
-        processRequest3();
-      } catch (err) {
-        console.log("Here we are", err);
-      }
-      async function processRequest3() {
+    case "/get/objectives":
+      getObjectivesAsync();
+      async function getObjectivesAsync() {
         try {
           res.json(await getObjectives());
         } catch (err) {
           console.log("DB Error:", err);
+          res.statusMessage = err;
+          res.sendStatus(404);
+        }
+      }
+      break;
+    case "/get/goals":
+      getGoalsAsync();
+      async function getGoalsAsync() {
+        try {
+          res.json(await getGoals());
+        } catch (err) {
+          console.log("DB Error:", err);
+          res.statusMessage = err;
           res.sendStatus(404);
         }
       }
       break;
     default:
-      //   res.status(404).sendFile(__dirname + "/unknown.html");
+      res.statusMessage = `Endpoint ${req.url} not supported`;
       res.sendStatus(404);
       break;
   }
 });
 
 app.post("*", (req, res) => {
-  console.log("Server Post Request:", req.url);
+  console.log("Server Post Request:", "url is ", req.url, "body is", req.body);
   switch (req.url) {
-    case "/AddObjective":
-      console.log(req.body);
+    case "/add/objective":
       addObjectiveAsync(req.body);
       async function addObjectiveAsync(data) {
         try {
@@ -54,11 +67,26 @@ app.post("*", (req, res) => {
           res.sendStatus(200);
         } catch (err) {
           console.log("DB Error:", err);
+          res.statusMessage = err;
+          res.sendStatus(404);
+        }
+      }
+      break;
+    case "/delete/objective":
+      deleteObjectiveAsync(req.body);
+      async function deleteObjectiveAsync(data) {
+        try {
+          await deleteObjective(data);
+          res.sendStatus(200);
+        } catch (err) {
+          console.log("DB Error:", err);
+          res.statusMessage = err;
+          res.sendStatus(404);
         }
       }
       break;
     default:
-      //   res.sendFile(__dirname + "/unknown.html");
+      res.statusMessage = `Endpoint ${req.url} not supported`;
       res.sendStatus(404);
       break;
   }
